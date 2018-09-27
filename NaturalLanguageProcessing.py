2502bfae75b0f1a8fd56309c50c3291baf37ad94
cnewman <clare_newman@ajg.com>
2018-09-27 16:48:31 +0100

    Added some functions to help clean up the data and added an aggregate on location.

--- NaturalLanguageProcessing.py
+++ NaturalLanguageProcessing.py
@@ -1,35 +1,66 @@
 # Imports the Google Cloud client library
+import math
+import re
+
+import numpy as np
+import pandas as pd
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
-import pandas as pd
-import numpy as np
-# import os
 
-# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "service_account.json"
+
+def clean_strings(statement):
+    upper_statement = str(statement)
+    upper_statement = upper_statement.upper()
+    upper_statement = re.sub(r"[^\w\s]", "", upper_statement)
+
+    print("Looking at:" + str(statement))
+
+    if upper_statement in ["NO", "NA", "NONE", "NOTHING"]:
+        return None
+    else:
+        return statement
+
+
+def int_to_boolean(num):
+    if not math.isnan(num):
+        return bool(int(num))
+    else:
+        return None
+
+
+def import_spreadsheet():
+    rows_to_import = None
+
+    csv = pd.read_csv("/Volumes/scLive/DATA 2018/LifePoint/HRD Survey Results/LifePoint Employee Survey 06092018.csv",
+                      nrows=rows_to_import)
+
+    csv = csv.rename(columns=lambda x: x.strip())
+
+    # Trim down to the columns we are interested in
+    csv = csv[
+        ["Gender", "Do you have any children?", "Length of service (years)", "Job title",
+         "Please select the facility in which you work.", "Can you tell us more?"]]
+
+    csv["Gender"].astype("category")
+    # print(csv.dtypes)
+    # csv["Can you tell us more?"].astype
+
+    # csv = csv["Do you have any children?"].apply(int_to_boolean)
+
+    # csv = csv["Can you tell us more?"].apply(clean_strings)
+
+    return csv
+
+
+data = import_spreadsheet()
 
 # Instantiates a client
 client = language.LanguageServiceClient()
 
-csv = pd.read_csv("/Volumes/scLive/DATA 2018/LifePoint/HRD Survey Results/LifePoint Employee Survey 06092018.csv")
-#                  nrows=25)
-
-csv = csv.rename(columns=lambda x: x.strip())
-# print(type(csv.to_string(columns=["Can you tell us more?"])))
-
-# with open('Advocacy_short.csv', 'r') as free_text:
-#    responses = []
-#    for response in free_text:
-#        responses.append(response)
-
-# responses = ["This is good", "I hate this", "It's OK"]
-
-responses = csv["Can you tell us more?"].values.tolist()
-# responses = [x for x in responses if str(x) != "nan"]
-# print(responses)
+responses = data["Can you tell us more?"].values.tolist()
 
 snippets = []
-analysis = ""
 
 for response in responses:
     snippet = types.Document(content=str(response), type=enums.Document.Type.PLAIN_TEXT)
@@ -41,18 +72,15 @@ sentiment = []
 for snippet in snippets:
     for index, sentence in enumerate(snippet.sentences):
         sentiment.append(sentence.sentiment.score)
-#       print("Sentiment of: {}\tfor sentence '{}'".format(round(sentence.sentiment.score, 2), sentence.text.content))
+    # print("Sentiment of: {}\tfor sentence '{}'".format(round(sentence.sentiment.score, 2), sentence.text.content))
 
-csv["Can you tell us more? (sentiment)"] = pd.Series(sentiment)
-csv = csv.drop("#", 1)
-csv["Gender"].astype("category")
+data["Can you tell us more? (sentiment)"] = pd.Series(sentiment)
 
-tell_us_more = csv[["Gender", "Do you have any children?", "Can you tell us more?", "Can you tell us more? (sentiment)"]]
-gender = tell_us_more.groupby("Gender")
+gender = data.groupby("Gender")
 print(gender["Can you tell us more? (sentiment)"].agg(np.mean))
-children = tell_us_more.groupby("Do you have any children?")
+
+children = data.groupby("Do you have any children?")
 print(children["Can you tell us more? (sentiment)"].agg(np.mean))
 
-# print(csv.columns.values)
-# print(tell_us_more.head())
-# print(csv.shape)
+location = data.groupby("Please select the facility in which you work.")
+print(location["Can you tell us more? (sentiment)"].agg(np.mean))
